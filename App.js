615c496375672a0d0953f3c71c13b1561cc10b89
yldrmsevval <yldrmsevval16@gmail.com>
2020-12-14 22:01:40 +0300

    ilk vers

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { View, Picker, StyleSheet, Button, TextInput, FlatList } from "react-native";
+
+let  state = {
+  data:[
+    {
+      bilim: "aefaefae"
+    },
+    {
+      sanat: "asdafae"
+    },
+  ],
+};
+
+const App = () => {
+  const [selectedValue, setSelectedValue] = useState("Seçiniz");
+  return (
+     
+    <View style={styles.container}>
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 150, width: 250 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+      
+        <Picker.Item label="Seçiniz" value="Seçiniz" />
+        <Picker.Item label="Bilim" value="bilim" />
+        <Picker.Item label="Sanat" value="sanat" />
+      </Picker>
+      <Button title="Gönder" color="purple"/>
+      <TextInput 
+      style={{width:200, height: 200, borderColor: 'gray', borderWidth: 5 }}/>
+      <FlatList></FlatList>
+      
+    </View> 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    alignItems: "center"
+  }
+});
+
+export default App;
